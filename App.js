d452eb846b699f799787d7f5f2c6248dc4f9a8ae
Ian Brash <iansbrash@gmail.com>
2020-11-01 16:06:14 -0600

    FINALLY GET REDUX-PERSIST WORKING

--- App.js
+++ App.js
@@ -3,20 +3,20 @@ import React from 'react';
 import { StyleSheet, SafeAreaView } from 'react-native';
 import { Provider } from 'react-redux'
 import store  from './redux/store'
-import { persistStore } from 'redux-persist/lib/storage'
+import { persistStore } from 'redux-persist'
 import { AppNavigator } from './components/TabNavigator';
 import 'react-native-gesture-handler';
 import { PersistGate } from 'redux-persist/integration/react'
 
-// let persistor = persistStore(store);
+let persistor = persistStore(store);
 
 export default function App() {
   return (
     <SafeAreaView style={{flex: 1, backgroundColor: 'white'}}>
       <Provider store={store}>
-          {/* <PersistGate persistor={persistor}> */}
+          <PersistGate persistor={persistor}>
                 <AppNavigator/>
-            {/* </PersistGate>` */}
+            </PersistGate>
         </Provider>
     </SafeAreaView>
     
--- redux/store.js
+++ redux/store.js
@@ -9,29 +9,29 @@ import {
     persistCombineReducers,
   } from 'redux-persist'
 import AsyncStorage from '@react-native-community/async-storage';
-//import { AsyncStorage } from 'react-native'
+// import { AsyncStorage } from 'react-native'
 
-// const persistConfig = {
-//     key: 'primary',
-//     version: 1,
-//     storage: AsyncStorage
-// }
+const persistConfig = {
+    key: 'primary',
+    version: 1,
+    storage: AsyncStorage
+}
 
-// const rootReducer = combineReducers({
-//     closet: closetReducer,
-//     outfits: outfitsReducer
-// })
-
-// const persistedReducer = persistReducer(persistConfig, rootReducer);
-
-
-// const store = createStore(persistedReducer)
-
-const store = configureStore({
-    reducer: {
-        closet: closetReducer,
-        outfits: outfitsReducer
-    }
+const rootReducer = combineReducers({
+    closet: closetReducer,
+    outfits: outfitsReducer
 })
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+
+const store = createStore(persistedReducer)
+
+// const store = configureStore({
+//     reducer: {
+//         closet: closetReducer,
+//         outfits: outfitsReducer
+//     }
+// })
 
 export default store
